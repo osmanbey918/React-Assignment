b494040cdaab9a7b9d74d9d67f35b9d9a5d3d562
Usman Khan <usman918khan@gmail.com>
2024-10-05 21:26:18 +0500

    setting-navbar

--- e-commerce-app/src/components/navbar/Navbar.js
+++ e-commerce-app/src/components/navbar/Navbar.js
@@ -12,15 +12,13 @@ const Navbar = () => {
   return (
     <nav className="navbar">
       <div className="navbar-logo">BrandName</div>
-      <ul className={`navbar-links ${isOpen ? 'open' : ''}`}>
+      <ul className={"navbar-links"} >
         <li><a href="#home">Home</a></li>
         <li><a href="#services">Services</a></li>
         <li><a href="#about">About</a></li>
         <li><a href="#contact">Contact</a></li>
       </ul>
-      <div className="navbar-toggle" onClick={toggleMenu}>
-        {/* {isOpen ? <FaTimes className="icon" /> : <FaBars className="icon" />} */}
-      </div>
+      
     </nav>
   );
 };
